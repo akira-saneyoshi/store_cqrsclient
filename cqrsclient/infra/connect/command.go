e2d06fccbef0226aba1af01ef573109b6f55d0ea
saneyoshi <saneyoshi@collabos.com>
2025-02-10 00:00:46 +0900

    add:TLSサポート、Goバイナリの更新

--- cqrsclient/infra/connect/command.go
+++ cqrsclient/infra/connect/command.go
@@ -1,9 +1,16 @@
 package connect
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"embed"
+
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 )
+
+//go:embed commandservice.pem
+var commandFile embed.FS
 
 // CommandServerと接続する
 type commandConnector struct {
@@ -16,12 +23,24 @@ func NewcommandConnector() ServerConnector {
 
 // CommandServiceとの接続を確立する
 func (c *commandConnector) Connect() (*grpc.ClientConn, error) {
+	// 埋め込まれた証明書を読み込む
+	pemData, err := commandFile.ReadFile("commandservice.pem")
+	if err != nil {
+		return nil, err
+	}
+	// 証明書のプールを作成し、PEMデータから証明書を追加する
+	cp := x509.NewCertPool()
+	if !cp.AppendCertsFromPEM(pemData) {
+		return nil, err
+	}
+	config := &tls.Config{ // クライアント用のTLS設定
+		RootCAs: cp,
+	}
+	creds := credentials.NewTLS(config)
 	server := "commandservice:8082" // サーバ名とポート番号
 	if conn, err := grpc.Dial(      // gRPCサーバとの接続を確立する
-		server, // 接続文字列
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		// 接続が確立するまで待つか、失敗した場合にエラーを即座に取得する
-		grpc.WithBlock(),
+		server,                               // 接続文字列
+		grpc.WithTransportCredentials(creds), // TLSを使用する
 	); err != nil {
 		return nil, err
 	} else {
--- cqrsclient/infra/connect/query.go
+++ cqrsclient/infra/connect/query.go
@@ -1,9 +1,16 @@
 package connect
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"embed"
+
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 )
+
+//go:embed queryservice.pem
+var queryFile embed.FS
 
 // QueryServerと接続する
 type queryConnector struct{}
@@ -15,12 +22,24 @@ func NewqueryConnector() ServerConnector {
 
 // QueryServiceとの接続を確立する
 func (ins *queryConnector) Connect() (*grpc.ClientConn, error) {
+	// 埋め込まれた証明書を読み込む
+	pemData, err := queryFile.ReadFile("queryservice.pem")
+	if err != nil {
+		return nil, err
+	}
+	// 証明書のプールを作成し、PEMデータから証明書を追加する
+	cp := x509.NewCertPool()
+	if !cp.AppendCertsFromPEM(pemData) {
+		return nil, err
+	}
+	config := &tls.Config{ // クライアント用のTLS設定
+		RootCAs: cp,
+	}
+	creds := credentials.NewTLS(config)
 	server := "queryservice:8083" // サーバ名とポート番号
 	if conn, err := grpc.Dial(    // gRPCサーバとの接続を確立する
-		server, // 接続文字列
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		// 接続が確立するまで待つか、失敗した場合にエラーを即座に取得する
-		grpc.WithBlock(),
+		server,                               // 接続文字列
+		grpc.WithTransportCredentials(creds), // TLSを使用する
 	); err != nil {
 		return nil, err
 	} else {
